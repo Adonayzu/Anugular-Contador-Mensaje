2f29ac167b15c457be7e51533d0dbca828d71ae2
Adonayzu <strlemuszu@upana.edu.gt>
2024-01-28 19:15:56 -0600

    Contador de numeros y mensaje

---
+++ src/app/hijo/hijo.component.ts
@@ -0,0 +1,37 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  templateUrl: './hijo.component.html',
+  styleUrls: ['./hijo.component.css']
+})
+export class HijoComponent {
+
+//de aqui saldra el mensaje al padre
+@Output() mensajeDesdeHijo = new EventEmitter<string>();
+@Output() incrementarDesdeHijo = new EventEmitter<void>();
+@Output() decrementarDesdeHijo = new EventEmitter<void>();
+
+mensaje: string = "";
+
+enviarMensaje(){
+  this.mensajeDesdeHijo.emit(this.mensaje )
+}
+
+incrementar(){
+  this.incrementarDesdeHijo.emit();
+}
+
+decrementar(){
+  this.decrementarDesdeHijo.emit();
+}
+
+
+
+  // EL HIJO RECIBE EL MENSAJE
+ //Este es el mensaje que recibe desde el padre
+ /*  @Input() recibeHijo?: string;   */
+
+
+
+}
